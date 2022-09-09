ae6c4316a4847b7f8d5bc57ec9ff6e64cd63acae
Gidoen Ofori <kgofori@gmail.com>
2022-09-09 12:19:43 -0700

    hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Description: print numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+	char alpha = 'a';
+
+	while (num < 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+
+	putchar('\n');
+	return (0);
+}
