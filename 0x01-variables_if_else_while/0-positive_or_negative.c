857c93b9a21f36279239ba6c4bf7bd8a1bc31de1
Gidoen Ofori <kgofori@gmail.com>
2022-09-09 11:16:16 -0700

    bety desc

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,13 @@
 #include <time.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: It check for type of reandom number.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
