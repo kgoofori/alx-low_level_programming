b59c1b9d65e830a25cc28088b586bac46563378c
Gidoen Ofori <kgofori@gmail.com>
2022-09-09 11:44:24 -0700

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Description: print alpa.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("abcdefghijklmnopqrstuvwxyz");
+
+	return (0);
+}
