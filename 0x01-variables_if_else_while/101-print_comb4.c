06c22b89dcf377dbad673a0d6f1dd674db1f9187
Gidoen Ofori <kgofori@gmail.com>
2022-09-09 13:18:13 -0700

    betty

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,11 +14,11 @@ int main(void)
 	int num2;
 	int num3;
 
-	for (num1 = 0; num1 < 8; num1++ )
+	for (num1 = 0; num1 < 8; num1++)
 	{
-		 for (num2 = num1 + 1; num2 < 9; num2++ )
-		 {
-			for (num3 = num2 + 1; num3 < 10; num3++ )
+		for (num2 = num1 + 1; num2 < 9; num2++)
+		{
+			for (num3 = num2 + 1; num3 < 10; num3++)
 			{
 				putchar(num1 + '0');
 				putchar(num2 + '0');
@@ -30,7 +30,7 @@ int main(void)
 					putchar(' ');
 				}
 			}
-		 }
+		}
 	}
 	putchar('\n');
 	return (0);
