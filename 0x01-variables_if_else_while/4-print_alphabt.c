4fb83bd691e3c1af40f44b0ba71eac865ed5cc09
Gidoen Ofori <kgofori@gmail.com>
2022-09-09 12:00:39 -0700

    ignore

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Description: print alpa.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		if ( alpha != 'e' || alpah != 'q')
+			putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
