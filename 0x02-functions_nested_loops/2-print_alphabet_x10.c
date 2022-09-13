cbce6f92c71f1c3d0731c77884d574e7fc99ba75
Gidoen Ofori <kgofori@gmail.com>
2022-09-13 12:50:30 -0700

    print 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -> prints alphabest
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+
+		_putchar('\n');
+
+	}
+
+}
