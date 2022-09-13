f39d2b443664c246ef932752fdf0bc0e431a150e
Gidoen Ofori <kgofori@gmail.com>
2022-09-13 15:04:15 -0700

    98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_to_98 -> checks for lowwercase
+ * @n: integer
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n <= 98)
+		{
+			if (n < 10)
+			{
+				_putchar(n + '0');
+			} else
+			{
+				_putchar(n / 10 + '0');
+				_putchar(n % 10 + '0');
+			}
+			n++;
+		}
+
+	} else
+	{
+		while (n >= 98)
+		{
+			if (n < 10)
+			{
+				_putchar(n + '0');
+			} else
+			{
+				_putchar(n / 10 + '0');
+				_putchar(n % 10 + '0')
+			}
+			n--;
+		}
+	}
+	_putchar('\n');
+}
