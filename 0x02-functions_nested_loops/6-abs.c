7d5d61d02b634cf7336b90061f5bc697203ecb65
Gidoen Ofori <kgofori@gmail.com>
2022-09-13 13:33:01 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+/**
+ * _abs -> checks for lowwercase
+ * @n: integer
+ * Return: 1 for lowercase and 0 for uppercase
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+
+	return (n);
+}
