6bc16f80723fed15e6a24702567c36a6dc139c9d
Gidoen Ofori <kgofori@gmail.com>
2022-09-13 13:09:02 -0700

    alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isalpha -> checks for lowwercase
+ * @c: integer
+ * Return: 1 for lowercase and 0 for uppercase
+ */
+int _isalpha(int c)
+{
+	return (((c <= 'a' && c >= 'z') || (c <= 'z' && c >= 'Z')))
+}
