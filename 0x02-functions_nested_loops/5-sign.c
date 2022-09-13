4daaccf06313f511ce5871f495841ecca38c2aec
Gidoen Ofori <kgofori@gmail.com>
2022-09-13 13:37:14 -0700

    ast

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,24 +1,11 @@
 #include "main.h"
 
 /**
- * print_sign -> checks for lowwercase
+ * print_last_digit -> checks for lowwercase
  * @n: integer
  * Return: 1 for lowercase and 0 for uppercase
  */
-int print_sign(int n)
+int print_last_digit(int n)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-
-	if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-
-	_putchar('-');
-	return (1);
+	_putchar(n % 10 + '0');
 }
---
+++ 0x02-functions_nested_loops/7-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_sign -> checks for lowwercase
+ * @n: integer
+ * Return: 1 for lowercase and 0 for uppercase
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+
+	_putchar('-');
+	return (1);
+}
