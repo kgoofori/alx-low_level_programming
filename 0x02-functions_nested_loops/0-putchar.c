ba523bd4aadd96d8ddb57925367ba012a0740bc9
Gidoen Ofori <kgofori@gmail.com>
2022-09-13 12:24:49 -0700

    header

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /* more headers goes there */
 
 /**
@@ -10,6 +10,14 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
