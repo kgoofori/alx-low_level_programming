6af3bb9095417b4368feddea4ede54dbf44eba41
Gidoen Ofori <kgofori@gmail.com>
2022-09-14 14:20:10 -0700

    110

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_times_table -> checks for lowwercase
+ * @n: int
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	int a, b, val;
+
+	for (a = 0; a <= n; a++)
+	{
+		for (b = 0; b <= n; b++)
+		{
+			val = a * b;
+			if (val < 10 && b != 0)
+			{
+				_putchar(' ');
+			}
+
+			if (b < n)
+			{
+				printf("%i, ", val);
+			} else 
+			{
+				printf("%i", val);
+			}
+		}
+
+		_putchar('\n');
+	}
+
+
+}
+
