4cd2b3136d66f1c605b78ecadcbe2dc303c7e6dc
Gidoen Ofori <kgofori@gmail.com>
2022-09-14 13:56:19 -0700

    times

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * times_table -> checks for lowwercase
+ * Return: void
+ */
+void times_table(void)
+{
+	int a, b, val;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			val = a * b;
+			if ( val < 10)
+			{
+				_putchar(val + '0');
+			}else
+			{
+				_putchar(val / 10 + '0');
+				_putchar(val % 10 + '0');
+			}
+
+			if (b < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+
+		_putchar('\n');
+	}
+
+
+}
+
