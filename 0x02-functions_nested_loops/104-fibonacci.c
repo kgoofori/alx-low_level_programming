ecc27157aa5d7ffaf11d015ec91aadc3c3443067
Gidoen Ofori <kgofori@gmail.com>
2022-09-14 15:35:30 -0700

    98

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Description: natural
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int i;
+	long int a = 0, b = 1,  val;
+
+	for (i = 0; i < 98; i++)
+	{
+		val = a + b;
+		a = b;
+		b = val;
+
+		printf("%lu", val);
+
+		if (i < 97)
+		{
+			printf(", ");
+		}
+	}
+
+	printf("\n");
+
+	return (0);
+}
