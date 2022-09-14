91d00af84f45d99ffbc027ae5c8e39a013b6e0f1
Gidoen Ofori <kgofori@gmail.com>
2022-09-14 14:57:41 -0700

    f

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,11 +9,9 @@
 void print_times_table(int n)
 {
 	int a, b, val;
-	if (n > 15)
-	{
-		break;
-	}
 
+	if (n < 15)
+	{
 	for (a = 0; a <= n; a++)
 	{
 		for (b = 0; b <= n; b++)
@@ -47,6 +45,7 @@ void print_times_table(int n)
 				}
 			}
 		}
+	}
 	}
 }
 
