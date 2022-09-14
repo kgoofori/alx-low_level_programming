61d7839751ba9c9c7149a1aacea831c4e23f7f35
Gidoen Ofori <kgofori@gmail.com>
2022-09-14 15:31:47 -0700

    sum

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Description: natural
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int i;
+	long int a = 0, b = 1, sum = 0, val;
+
+	for (i = 0; i < 50; i++)
+	{
+		val = a + b;
+		a = b;
+		b = val;
+
+		if (val % 2 == 0)
+		{
+			sum += val;
+		}
+	}
+
+	printf("%lu\n", sum);
+
+	return (0);
+}
