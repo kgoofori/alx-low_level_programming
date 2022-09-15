856e228689e41f79acbf1c356c6a29fe88db6f8f
Gidoen Ofori <kgofori@gmail.com>
2022-09-15 13:40:13 -0700

    zero

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+}
+
