68776eabd023dc3ed8f5bcf63afa73baa511d704
Gidoen Ofori <kgofori@gmail.com>
2022-09-16 02:06:22 -0700

    s2q

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_square -> checks for lowwercase
+ * @size: int
+ * Return: void
+ */
+void print_square(int size)
+{
+	int i = 0;
+	int x;
+
+	while (i < size)
+	{
+		for (x = 0; x < i ; x++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+		i++;
+	}
+
+}
