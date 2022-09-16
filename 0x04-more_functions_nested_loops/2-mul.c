5d1de63d2df8ad008d8f1a996be1e24abe5fe91b
Gidoen Ofori <kgofori@gmail.com>
2022-09-16 01:08:51 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul -> checks for lowwercase
+ * @a: integer
+ * @b: integer
+ * Return: 0 for lowercase and 1 for uppercase
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
