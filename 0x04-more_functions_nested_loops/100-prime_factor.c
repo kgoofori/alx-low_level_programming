71e21bd82d8db3d379cbc5a7386220871c1d1d4f
Gidoen Ofori <kgofori@gmail.com>
2022-09-16 16:13:38 -0700

    num

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,22 +1,39 @@
-include <stdio.h>
-#include <math.h>
+#include <stdio.h>
+
 /**
  * main - prime numbers
  *
- * Return: Always 0.
+ * Return: 0
  */
 int main(void)
 {
-
-	unsigned int long n = 612852475143, a = (int) sqrt(n);
-
-	while (n % a == 0)
+	int long i, j, num, isPrime;
+	num = 612852475143;
+	for (i = 2; i <= num; i++)
 	{
+		/* Check 'i' for factor of num */
+		if (num % i == 0)
+		{
+			/* Check 'i' for Prime */
+			isPrime = 1;
+			for (j = 2; j <= i / 2; j++)
+			{
+				if (i % j == 0)
+				{
+					isPrime = 0;
+					break;
+				}
+			}
 
-		printf("%lu \n", n / a);
-		a++;
+			/* If 'i' is Prime number and factor of num */
+			if (isPrime == 1)
+			{
+				printf("%lu", i);
+			}
+		}
 	}
 
+	printf("\n");
 
 	return (0);
 }
