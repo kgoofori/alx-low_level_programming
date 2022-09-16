3c495357c57b7253dff35a858b2ab5974b62407c
Gidoen Ofori <kgofori@gmail.com>
2022-09-16 01:54:22 -0700

    7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagonal -> checks for lowwercase
+ * @n: int
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		_putchar(' ');
+		i++;
+	}
+
+	_putchar('\');
+	_putchar('\n');
+
+}
