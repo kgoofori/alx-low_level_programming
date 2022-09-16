0159ab55b84d8e8c945c683c485bd9c46e771fbd
Gidoen Ofori <kgofori@gmail.com>
2022-09-16 01:04:33 -0700

    digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit -> checks for lowwercase
+ * @c: integer
+ * Return: 0 for lowercase and 1 for uppercase
+ */
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
