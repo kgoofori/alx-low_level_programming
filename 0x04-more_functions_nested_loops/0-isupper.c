2420402c5e96b14a6edbb6304a2e18a71c33b021
Gidoen Ofori <kgofori@gmail.com>
2022-09-16 00:57:27 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper -> checks for lowwercase
+ * @c: integer
+ * Return: 0 for lowercase and 1 for uppercase
+ */
+int _isupper(int c)
+{
+	if (c >= 97)
+	{
+		return (0);
+	}
+
+	return (1);
+
+}
