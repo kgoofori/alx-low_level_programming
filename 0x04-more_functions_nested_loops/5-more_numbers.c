486426ba2619e690409b9c7543d32fea73fb5d5e
Gidoen Ofori <kgofori@gmail.com>
2022-09-16 01:42:17 -0700

    r

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * more_numbers -> checks for lowwercase
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i = 0;
+	int x;
+	int rem;
+
+	while (i < 10)
+	{
+		for (x = 0; x < 15; x++)
+		{
+			rem = x;
+			if (x > 9)
+			{
+				rem = x % 10;
+				_putchar(x / 10 + '0');
+			}
+
+			_putchar('0' + x);
+		}
+
+		_putchar('\n');
+		i++;
+	}
+
+}
