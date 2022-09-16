a6c994066092b19220e289995e695ed7b736189b
Gidoen Ofori <kgofori@gmail.com>
2022-09-16 01:14:28 -0700

    0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers -> checks for lowwercase
+ * Return: void 
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%i", i);
+		i++;
+	}
+
+}
