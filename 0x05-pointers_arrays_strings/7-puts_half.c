b26d6d278cea60ca76e9b7f73716ca3fb620b14d
Gidoen Ofori <kgofori@gmail.com>
2022-09-19 16:22:36 -0700

    print

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,25 +3,23 @@
 /**
  * puts_half - find the length of a string
  * @s: string pointer
+ * Description: desc
  * Return: int
 */
 
 void puts_half(char *str)
 {
 
-	int i = 1;
+	int i = 0;
 
 	while (str[i])
 	{
 		i++;
 	}
 
-	i = i / 2;
-
-	while (str[i])
+	for (i /= 2; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
-		i++;
 	}
 
 	_putchar('\n');
