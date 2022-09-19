f9fbaf7e0c69a09f6a40db679069979438e2b8f6
Gidoen Ofori <kgofori@gmail.com>
2022-09-19 16:10:30 -0700

    pit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * puts2 - prints a string
+ * @str: pointer
+ * Return: void
+*/
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+
+		i++;
+	}
+	_putchar('\n');
+}
