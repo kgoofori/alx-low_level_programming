b29ca4331f3353b2a49ea040dbd6afc0b9191d83
Gidoen Ofori <kgofori@gmail.com>
2022-09-19 16:16:29 -0700

    hallf

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts_half - find the length of a string
+ * @s: string pointer
+ * Return: int
+*/
+
+void puts_half(char *str)
+{
+
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+
+	i = i / 2;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+
+}
