5ec66c086407fed02c565053565fd81610f16bf3
Gidoen Ofori <kgofori@gmail.com>
2022-09-19 16:45:17 -0700

    copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - print `n` elements of an array
+ * @dest: pointer destination
+ * @src: source
+ * Description: print number with space and comma.
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	return (dest);
+}
