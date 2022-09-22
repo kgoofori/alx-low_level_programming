77d9b0d4081ca130cba0541931d044cc5829a61f
Gidoen Ofori <kgofori@gmail.com>
2022-09-22 14:31:10 -0700

    truncate

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strncat - truncat two strings
+ * @dest: pointer
+ * @src: pointer
+ * @n: int
+ * Return: void
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int a = -1, i = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	do {
+		a++;
+		dest[i] = src[a];
+		i++;
+	} while (i < n && src[a] != '\0');
+
+	return (dest);
+}
