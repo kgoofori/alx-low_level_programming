f082439018763ed029441d1f9d158710d04d3ba2
Gidoen Ofori <kgofori@gmail.com>
2022-09-22 17:06:30 -0700

    betty

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,6 +9,7 @@
 char *leet(char *str)
 {
 	int i;
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] == 'a' || str[i] == 'A')
@@ -16,7 +17,7 @@ char *leet(char *str)
 		else if (str[i] == 'e' || str[i] == 'E')
 			str[i] = '3';
 		else if (str[i] == 'o' || str[i] == 'O')
-		       	str[i] = '0';
+			str[i] = '0';
 		else if (str[i] == 't' || str[i] == 'T')
 			str[i] = '7';
 		else if (str[i] == 'l' || str[i] == 'L')
