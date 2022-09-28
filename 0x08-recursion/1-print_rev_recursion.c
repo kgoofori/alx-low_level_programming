acf2832a73a5d91d52b10a28d64de84b913129c7
Gidoen Ofori <kgofori@gmail.com>
2022-09-28 15:12:36 -0700

    rev

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - loop over char*
+ * @s: pointer
+ * Return: void
+ */
+
+
+void _print_rev_recursion(char *s)
+{
+
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+
+}
