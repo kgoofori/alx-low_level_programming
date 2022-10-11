c79da9d8bc105382f5a8daeb2609a2a7e9e983fc
Gidoen Ofori <kgofori@gmail.com>
2022-10-11 06:17:54 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog
+ * @name: string
+ * @age: integer
+ * @owner: string
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
